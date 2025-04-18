99f64409cf1164df5e6881c6ffb9043a94eac49a
DinhHuuPhuong <85395125+dinhhuuphuong@users.noreply.github.com>
2025-04-18 20:14:50 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 import cookieParser from 'cookie-parser'
 import { app, server } from './sockets/socket'
 
+const PORT = process.env.PORT ?? env.APP_PORT ?? 8022
+
 //Fix Cache from disk của ExpressJS
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store')
@@ -24,7 +26,7 @@ app.use('/api/v1', APIs_V1)
 // Middleware xử lý lỗi tập trung trong ứng dụng Back-end NodeJS (ExpressJS)
 app.use(errorHandlingMiddleware)
 
-server.listen(env.APP_PORT, env.APP_HOST, () => {
+server.listen(PORT, env.APP_HOST, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  console.log(`Server running at http://${env.APP_HOST}:${PORT}/`)
 })
